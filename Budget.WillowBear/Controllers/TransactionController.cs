df816e5f455d8483391c96eb0480db738e81a5e4
Stuart Willows <SJWillows91@outlook.com>
2022-12-09 03:02:05 +0000

    Added CRUD to Transac and Cat controllers

--- Budget.WillowBear/Controllers/TransactionController.cs
+++ Budget.WillowBear/Controllers/TransactionController.cs
@@ -2,6 +2,7 @@
 using Budget.WillowBear.Data;
 using Budget.WillowBear.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Budget.WillowBear.Controllers
 {
@@ -16,13 +17,107 @@ namespace Budget.WillowBear.Controllers
             _context = context;
         }
 
+        // GET: api/Transaction
+        //
         [HttpGet]
-        public IEnumerable<Transaction> GetAll()
+        public async Task<IEnumerable<Transaction>> GetAllTransactions()
         {
-            return _context.Transactions;
+            var transactions = new List<Transaction>();
+            transactions = await _context.Transactions.ToListAsync();
+
+            return transactions;
         }
 
+        // GET: api/Transaction/{id}
+        //
+        [HttpGet("{id}")]
+        public async Task<Transaction?> GetTransactionByIdAsync(int id)
+        {
+            // Attempt to find transaction by id
+            Transaction? transaction = await _context.Transactions.FirstOrDefaultAsync(t => t.Id == id);
 
+            // Check if transaction is null
+            if (transaction == null)
+            {
+                Console.WriteLine("Transaction not found");
+                return null;
+            }
+
+            Console.WriteLine($"transaction {transaction!.Id} Retrieved ");
+            return transaction;
+        }
+
+        // POST: /create
+        //
+        public async Task<IActionResult> AddTransaction(Transaction transaction)
+        {
+            try
+            {
+                _context.Transactions.AddAsync(transaction);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return BadRequest("Transaction not added");
+            }
+        }
+
+        // PUT: /edit
+        //
+        [HttpPut]
+        [Route("edit/{id}")]
+        public async Task<IActionResult> EditTransaction(Transaction transaction, int id)
+        {
+
+            // Check if transaction exists
+            if (!_context.Transactions.Any(t => t.Id == id))
+            {
+                return NotFound("Transaction not found");
+            }
+
+            try
+            {
+                _context.Entry(transaction).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return BadRequest("Transaction not updated");
+            }
+        }
+
+        // DELETE: /delete{id}
+        //
+        [HttpDelete("{id}")]
+        [Route("delete/{id}")]
+        public async Task<IActionResult> DeleteTransaction(int id)
+        {
+            var transaction = await _context.Transactions.FirstOrDefaultAsync(t => t.Id == id);
+
+            // Check if transaction exists
+            //
+            if (transaction == null)
+            {
+                return NotFound("Transaction not found");
+            }
+
+            try
+            {
+                Console.WriteLine("Deleting transaction");
+                _context.Transactions.Remove(transaction);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return BadRequest("Transaction not deleted");
+            }
+        }
 
     }
 }
--- Budget.WillowBear/Data/BudgetDbContext.cs
+++ Budget.WillowBear/Data/BudgetDbContext.cs
@@ -16,192 +16,19 @@ namespace Budget.WillowBear.Data
             modelBuilder.ApplyConfiguration(new CategoryConfiguration());
         }
 
-        public DbSet<Transaction> Transactions { get; set; }
-        public DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Transaction> Transactions { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
 
 
         // CATEGORY METHODS //
 
-        // POST
-        //
-        public void AddCategory(Category category)
-        {
-            Categories.Add(category);
-            SaveChanges();
-        }
-
-        // GET ALL
-        //
-        public IEnumerable<Category> GetAllCategories()
-        {
-            return Categories;
-        }
-
-        // GET BY ID
-        //
-        public Category? GetCategoryById(int id)
-        {
-            // Attempt to find category by id
-            //
-            Category? category = Categories.FirstOrDefault(c => c.Id == id);
-
-            // Check if category is null
-            //
-            if (category == null)
-            {
-                Console.WriteLine("Category not found");
-                return null;
-            }
-
-            Console.WriteLine($"category {category!.Id} Retrieved ");
-            return category;
-        }
-
-        // PUT
-        //
-        public bool UpdateCategory(Category category)
-        {
-            // Check if category is null
-            //
-            if (category == null)
-            {
-                Console.WriteLine("Category not found");
-                return false;
-            }
-
-            // Check if category exists
-            //
-            if (!Categories.Any(c => c.Id == category.Id))
-            {
-                Console.WriteLine("Category not found");
-                return false;
-            }
-
-            try
-            {
-                Categories.Update(category);
-                SaveChanges();
-                return true;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return false;
-            }
-        }
-
-        // DELETE
-        // Deletes category and all transactions associated with category
-        //
-        public bool DeleteCategoriesAndTransactions(int id)
-        {
-            var category = Categories.FirstOrDefault(c => c.Id == id);
-
-            // Check if category exists
-            //
-            if (category == null)
-            {
-                Console.WriteLine("Category not found");
-                return false;
-            }
-
-            try
-            {
-                // Delete category
-                Categories.Remove(category);
-
-                // Check if category has any transactions
-                if (Transactions.Any(t => t.CategoryId == category.Id))
-                {
-                    // Delete all transactions associated with category
-                    Console.WriteLine("Deleting transactions");
-                    Transactions.RemoveRange(Transactions.Where(t => t.CategoryId == category.Id));
-                }
-                SaveChanges();
-                return true;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return false;
-            }
-        }
-
         // TRANSACTION METHODS //
 
-        // POST
-        //
-        public bool AddTransaction(Transaction transaction)
-        {
-            try
-            {
-                Transactions.Add(transaction);
-                SaveChanges();
-                return true;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Transaction not added");
-                Console.WriteLine(e.Message);
-                return false;
-            }
-        }
 
-        // GET ALL
-        //
-        public IEnumerable<Transaction> GetAllTransactions()
-        {
-            return Transactions;
-        }
 
-        // GET BY ID
-        //
-        public Transaction? GetTransactionById(int id)
-        {
-            // Attempt to find transaction by id
-            Transaction? transaction = Transactions.FirstOrDefault(t => t.Id == id);
 
-            // Check if transaction is null
-            if (transaction == null)
-            {
-                Console.WriteLine("Transaction not found");
-                return null;
-            }
 
-            Console.WriteLine($"transaction {transaction!.Id} Retrieved ");
-            return transaction;
-        }
 
-        // PUT
-        //
-        public bool EditTransaction(Transaction transaction)
-        {
-            // Check if transaction is null
-            if (transaction == null)
-            {
-                Console.WriteLine("Transaction not found");
-                return false;
-            }
-
-            // Check if transaction exists
-            if (!Transactions.Any(t => t.Id == transaction.Id))
-            {
-                Console.WriteLine("Transaction not found");
-                return false;
-            }
-
-            try
-            {
-                Transactions.Update(transaction);
-                SaveChanges();
-                return true;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return false;
-            }
-        }
 
 
         // DELETE
@@ -222,7 +49,6 @@ namespace Budget.WillowBear.Data
             {
                 Console.WriteLine("Deleting transaction");
                 Transactions.Remove(transaction);
-                SaveChanges();
                 Console.WriteLine("Transaction deleted");
                 return true;
             }
