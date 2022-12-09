5f0eda2e0fbf92d755c991998e309033f77a79fb
Stuart Willows <SJWillows91@outlook.com>
2022-12-09 01:10:58 +0000

    Added Category controller crud

--- Budget.WillowBear/Controllers/CategoryController.cs
+++ Budget.WillowBear/Controllers/CategoryController.cs
@@ -20,17 +20,85 @@ namespace Budget.WillowBear.Controllers
             _context = context;
         }
 
+        // GET: api/Category
+        //
         [HttpGet]
         public IEnumerable<Category> GetAll()
         {
             return _context.Categories;
         }
 
+        // POST: api/Category
         [HttpPost]
+        [Route("create")]
         public IActionResult Create(Category category)
         {
             _context.AddCategory(category);
             return Ok();
+        }
+
+        // PUT: api/Category/edit/{id}
+        [HttpPut]
+        [Route("edit/{id}")]
+        public IActionResult EditCategory(Category category)
+        {
+            // Check if category is null
+            //
+            if (category == null)
+            {
+                return BadRequest("No category provided");
+            }
+
+            // Check if category id matches route id
+            //
+            if (category.Id != category.Id)
+            {
+                return BadRequest("Category Id mismatch");
+            }
+
+            // Check if category exists
+            //
+            if (!_context.Categories.Any(c => c.Id == category.Id))
+            {
+                return NotFound("Category not found");
+            }
+
+            try
+            {
+                _context.Update(category);
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return BadRequest("Error updating category");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+
+            // Check if category exists
+            //
+            if (category == null)
+            {
+                return NotFound("Category not found");
+            }
+
+            try
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return BadRequest("Error deleting category");
+            }
         }
     }
 }
