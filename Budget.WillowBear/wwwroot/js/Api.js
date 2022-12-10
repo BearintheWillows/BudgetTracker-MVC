c0f0fc0faf54696095fd512516685cb952f24e7f
Stuart Willows <SJWillows91@outlook.com>
2022-12-10 01:02:26 +0000

    Created JS Files. Implemented API Calls.

---
+++ Budget.WillowBear/wwwroot/js/Api.js
@@ -0,0 +1,122 @@
+class Api {
+    constructor() {
+        this.TransactionsUrl = "api/Transaction";
+        this.CategoriesUrl = "api/Category";
+    }
+
+    // Transaction API Methods //
+
+    // Get all transactions
+    //
+    async getTransactions() {
+        const response = await fetch(this.TransactionsUrl);
+        const data = await response.json();
+        return data;
+    }
+
+    // Get a single transaction
+    //
+    async getTransaction(id) {
+        const response = await fetch(`${this.TransactionsUrl}/${id}`);
+        const data = await response.json();
+        return data;
+    }
+
+    // Create a new transaction
+    //
+    async createTransaction(transaction) {
+        const response = await fetch(`${this.TransactionsUrl}/${create}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(transaction)
+        });
+        const data = await response.json();
+        return data;
+    }
+
+    // Update an existing transaction
+    //
+    async updateTransaction(id, transaction) {
+        const response = await fetch(`${this.TransactionsUrl}/update/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(transaction)
+        });
+        const data = await response.json();
+        return data;
+    }
+
+    // Delete a transaction
+    //
+    async deleteTransaction(id) {
+        const response = await fetch(`${this.TransactionsUrl}/delete/${id}`, {
+            method: 'DELETE'
+        });
+        const data = await response.json();
+        return data;
+    }
+
+    // Category API Methods //
+
+    // Get all categories
+    //
+    async getCategories() {
+        const response = await fetch(this.CategoriesUrl);
+        const data = await response.json();
+        return data;
+    }
+
+    // Get a single category
+    //
+    async getCategory(id) {
+        const response = await fetch(`${this.CategoriesUrl}/${id}`);
+        const data = await response.json();
+        return data;
+    }
+
+    // Create a new category
+    //
+    async createCategory(category) {
+        const response = await fetch(`${this.CategoriesUrl}/create`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(category)
+        });
+        const data = await response.json();
+        return data;
+    }
+
+    // Update an existing category
+    //
+    async updateCategory(id, category) {
+        const response = await fetch(`${this.CategoriesUrl}/update/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(category)
+        });
+        const data = await response.json();
+        return data;
+    }
+
+    // Delete a category
+    //
+    async deleteCategory(id) {
+        const response = await fetch(`${this.CategoriesUrl}/delete/${id}`, {
+            method: 'DELETE'
+        });
+        const data = await response.json();
+        return data;
+    }
+
+
+
+
+}
---
+++ Budget.WillowBear/wwwroot/js/Categories.js
@@ -0,0 +1,8 @@
+
+class Categories {
+    constructor() {
+        this.categories = [];
+    }
+
+
+}
