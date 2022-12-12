9338f69849b208810e451fa9dcb47d3e64b8a72e
Stuart Willows <SJWillows91@outlook.com>
2022-12-12 05:34:45 +0000

    Stated adding category api requests

--- Budget.WillowBear/Controllers/CategoryController.cs
+++ Budget.WillowBear/Controllers/CategoryController.cs
@@ -40,31 +40,11 @@ namespace Budget.WillowBear.Controllers
 
         // GET: api/Category/{id}
         //
-        [HttpGet("{id}")]
+        [Route("{id}")]
         public async Task<ActionResult<CategoryDTO?>> GetCategoryById(int id)
         {
             // Attempt to find category by id and convert to DTO
             CategoryDTO? category = await _context.Categories.Where(c => c.Id == id).Select(c => new CategoryDTO
-            {
-                Id = c.Id,
-                Name = c.Name,
-                Description = c.Description,
-            }).FirstOrDefaultAsync();
-
-            // Check if category is null
-            if (category == null)
-            {
-                return NotFound("Category not found in Database");
-            }
-
-            return category;
-        }
-
-        [HttpGet("{name}")]
-        public async Task<ActionResult<CategoryDTO?>> GetCategoryByName(string name)
-        {
-            // Attempt to find category by name and convert to DTO
-            CategoryDTO? category = await _context.Categories.Where(c => c.Name == name).Select(c => new CategoryDTO
             {
                 Id = c.Id,
                 Name = c.Name,
--- Budget.WillowBear/Scripts/app.ts
+++ Budget.WillowBear/Scripts/app.ts
@@ -14,3 +14,38 @@ interface ICategory {
     name: string;
     description: string;
 }
+
+// Category Class
+class CategoryDataAccess {
+    readonly baseUrl: string = '/api/category';
+    //Get all categories
+    async getAll(): Promise<ICategory[]> {
+        const response = await fetch(this.baseUrl);
+        const data = await response.json();
+        return data;
+    }
+
+    //Get by Id
+    async getById(id: number): Promise<ICategory> {
+        const response = await fetch(`${this.baseUrl}/id/${id}`);
+        const data = await response.json();
+        return data;
+    }
+}
+
+class Transaction {
+    //TODO: Implement Transaction class
+}
+
+
+
+
+let categoryDataAccess = new CategoryDataAccess();
+
+categoryDataAccess.getAll().then((categories) => {
+    console.log(categories);
+});
+
+categoryDataAccess.getById(3).then((category) => {
+    console.log(category);
+});
